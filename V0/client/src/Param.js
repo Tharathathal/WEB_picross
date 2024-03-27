b28ca8c1e52683931a3dfd0288e93ef2387e2f31
Squaresponge-Blob <mailys.duthilleul@gmail.com>
2024-03-27 18:03:54 +0100

    Affichage de la  page de début de jeu

---
+++ V0/client/src/Param.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Radio, RadioGroup, FormControl, FormControlLabel, FormLabel } from '@mui/material';
+
+function GetParam({ setSize, setPicture }) {
+    const [value, setValue] = useState("");
+    
+    const handleChange = (event) => {
+        setValue(event.target.value); // Update the state with the selected value
+        setSize(parseInt(event.target.value)); // Convert the selected value to an integer and set the size
+      }
+
+    return (
+      <div>
+        <h2>Select Picross Options</h2>
+          <FormControl>
+            <FormLabel id="difficulty-control">Difficulty Level</FormLabel>
+            <RadioGroup
+                aria-labelledby="difficulty-control"
+                name="difficulty-control"
+                value={value}
+                onChange={handleChange}
+            >
+                <FormControlLabel value="9" control={<Radio />} label="Easy" />
+                <FormControlLabel value="25" control={<Radio />} label="Normal" />
+                <FormControlLabel value="64" control={<Radio />} label="Hard" />
+            </RadioGroup>
+            </FormControl>
+        <br />
+        <label>
+          Upload Picture:
+          {/* Implement your picture upload logic */}
+        </label>
+      </div>
+    );
+  }
+
+export default GetParam;
