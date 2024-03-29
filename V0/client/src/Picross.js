d8df2b5becd0ea248c63152e18c279daf61eef5a
Tharathathal <thaliacharton@free.fr>
2024-03-29 11:18:27 +0100

    mise en forme

--- V0/client/src/Picross.js
+++ V0/client/src/Picross.js
@@ -218,6 +218,7 @@ function Game() {
           </div>
         ) : (
           <>
+          <form className='form-box'>
             <Hearts numErrors={errors.reduce((acc, curr) => acc + (curr ? 1 : 0), 0)} />
             <div className="container">
               <div className="row-wrapper">
@@ -245,6 +246,7 @@ function Game() {
             <div className="switch">
               <MySwitch checked={blackIsPlayed} onChange={handleSwitchChange} />
             </div>
+            </form>
           </>
         )}
       </>
